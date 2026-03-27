3b4c7ee78beed2e6a22580d597a86f6a5d494139
Prophet35208 <taran.danil03@mail.ru>
2026-03-27 14:11:29 +0700

    Так, ошибка в тестах. Исправил

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -1,106 +1,126 @@
 #include <gtest/gtest.h>
-#include "calculator.h"
+#include "Calculator.h"
 
-class CalculatorTest : public ::testing::Test {
+class CalculatorTest : public ::testing::Test
+{
 protected:
     Calculator calc;
 };
 
-// Тесты для сложения
-TEST_F(CalculatorTest, AddPositiveNumbers) {
+// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TEST_F(CalculatorTest, AddPositiveNumbers)
+{
     EXPECT_DOUBLE_EQ(calc.add(2, 3), 5);
     EXPECT_DOUBLE_EQ(calc.add(10, 20), 30);
 }
 
-TEST_F(CalculatorTest, AddNegativeNumbers) {
+TEST_F(CalculatorTest, AddNegativeNumbers)
+{
     EXPECT_DOUBLE_EQ(calc.add(-2, -3), -5);
     EXPECT_DOUBLE_EQ(calc.add(-10, 5), -5);
 }
 
-TEST_F(CalculatorTest, AddZero) {
+TEST_F(CalculatorTest, AddZero)
+{
     EXPECT_DOUBLE_EQ(calc.add(5, 0), 5);
     EXPECT_DOUBLE_EQ(calc.add(0, 5), 5);
     EXPECT_DOUBLE_EQ(calc.add(0, 0), 0);
 }
 
-// Тесты для вычитания
-TEST_F(CalculatorTest, SubtractPositiveNumbers) {
+// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TEST_F(CalculatorTest, SubtractPositiveNumbers)
+{
     EXPECT_DOUBLE_EQ(calc.subtract(5, 3), 2);
     EXPECT_DOUBLE_EQ(calc.subtract(10, 4), 6);
 }
 
-TEST_F(CalculatorTest, SubtractNegativeNumbers) {
+TEST_F(CalculatorTest, SubtractNegativeNumbers)
+{
     EXPECT_DOUBLE_EQ(calc.subtract(-5, -3), -2);
     EXPECT_DOUBLE_EQ(calc.subtract(5, -3), 8);
 }
 
-// Тесты для умножения
-TEST_F(CalculatorTest, MultiplyPositiveNumbers) {
+// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TEST_F(CalculatorTest, MultiplyPositiveNumbers)
+{
     EXPECT_DOUBLE_EQ(calc.multiply(2, 3), 6);
     EXPECT_DOUBLE_EQ(calc.multiply(4, 5), 20);
 }
 
-TEST_F(CalculatorTest, MultiplyByZero) {
+TEST_F(CalculatorTest, MultiplyByZero)
+{
     EXPECT_DOUBLE_EQ(calc.multiply(5, 0), 0);
     EXPECT_DOUBLE_EQ(calc.multiply(0, 5), 0);
 }
 
-TEST_F(CalculatorTest, MultiplyNegativeNumbers) {
+TEST_F(CalculatorTest, MultiplyNegativeNumbers)
+{
     EXPECT_DOUBLE_EQ(calc.multiply(-2, 3), -6);
     EXPECT_DOUBLE_EQ(calc.multiply(-2, -3), 6);
 }
 
-// Тесты для деления
-TEST_F(CalculatorTest, DividePositiveNumbers) {
+// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TEST_F(CalculatorTest, DividePositiveNumbers)
+{
     EXPECT_DOUBLE_EQ(calc.divide(6, 2), 3);
     EXPECT_DOUBLE_EQ(calc.divide(10, 4), 2.5);
 }
 
-TEST_F(CalculatorTest, DivideByZero) {
+TEST_F(CalculatorTest, DivideByZero)
+{
     EXPECT_THROW(calc.divide(5, 0), std::invalid_argument);
 }
 
-TEST_F(CalculatorTest, DivideNegativeNumbers) {
+TEST_F(CalculatorTest, DivideNegativeNumbers)
+{
     EXPECT_DOUBLE_EQ(calc.divide(-6, 2), -3);
     EXPECT_DOUBLE_EQ(calc.divide(-6, -2), 3);
 }
 
-// Тесты для возведения в степень
-TEST_F(CalculatorTest, PowerPositiveExponent) {
+// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TEST_F(CalculatorTest, PowerPositiveExponent)
+{
     EXPECT_DOUBLE_EQ(calc.power(2, 3), 8);
     EXPECT_DOUBLE_EQ(calc.power(5, 2), 25);
 }
 
-TEST_F(CalculatorTest, PowerZeroExponent) {
+TEST_F(CalculatorTest, PowerZeroExponent)
+{
     EXPECT_DOUBLE_EQ(calc.power(5, 0), 1);
     EXPECT_DOUBLE_EQ(calc.power(0, 0), 1);
 }
 
-TEST_F(CalculatorTest, PowerNegativeExponent) {
+TEST_F(CalculatorTest, PowerNegativeExponent)
+{
     EXPECT_DOUBLE_EQ(calc.power(2, -1), 0.5);
     EXPECT_DOUBLE_EQ(calc.power(4, -2), 0.0625);
 }
 
-// Тесты для факториала
-TEST_F(CalculatorTest, FactorialOfZero) {
+// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TEST_F(CalculatorTest, FactorialOfZero)
+{
     EXPECT_EQ(calc.factorial(0), 1);
 }
 
-TEST_F(CalculatorTest, FactorialOfOne) {
+TEST_F(CalculatorTest, FactorialOfOne)
+{
     EXPECT_EQ(calc.factorial(1), 1);
 }
 
-TEST_F(CalculatorTest, FactorialOfPositiveNumber) {
+TEST_F(CalculatorTest, FactorialOfPositiveNumber)
+{
     EXPECT_EQ(calc.factorial(5), 120);
     EXPECT_EQ(calc.factorial(7), 5040);
 }
 
-TEST_F(CalculatorTest, FactorialOfNegativeNumber) {
+TEST_F(CalculatorTest, FactorialOfNegativeNumber)
+{
     EXPECT_THROW(calc.factorial(-1), std::invalid_argument);
 }
 
-// Тесты для проверки простых чисел
-TEST_F(CalculatorTest, PrimeNumbers) {
+// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+TEST_F(CalculatorTest, PrimeNumbers)
+{
     EXPECT_TRUE(calc.isPrime(2));
     EXPECT_TRUE(calc.isPrime(3));
     EXPECT_TRUE(calc.isPrime(5));
@@ -109,7 +129,8 @@ TEST_F(CalculatorTest, PrimeNumbers) {
     EXPECT_TRUE(calc.isPrime(13));
 }
 
-TEST_F(CalculatorTest, NotPrimeNumbers) {
+TEST_F(CalculatorTest, NotPrimeNumbers)
+{
     EXPECT_FALSE(calc.isPrime(1));
     EXPECT_FALSE(calc.isPrime(4));
     EXPECT_FALSE(calc.isPrime(6));
@@ -118,7 +139,8 @@ TEST_F(CalculatorTest, NotPrimeNumbers) {
     EXPECT_FALSE(calc.isPrime(10));
 }
 
-TEST_F(CalculatorTest, PrimeNumbersLarge) {
+TEST_F(CalculatorTest, PrimeNumbersLarge)
+{
     EXPECT_TRUE(calc.isPrime(97));
     EXPECT_TRUE(calc.isPrime(101));
 }
